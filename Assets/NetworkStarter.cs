837772b479f7fc1c4a03f9a248b1068023535c64
Emil Magnusson <Candyynnz@gmail.com>
2023-02-07 15:07:23 +0100

    Implement Voice for players.

--- Assets/NetworkStarter.cs
+++ Assets/NetworkStarter.cs
@@ -1,14 +1,31 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
 using Unity.Netcode;
+using VivoxUnity;
 
 public class NetworkStarter : MonoBehaviour
 {
+    VivoxVoiceManager voiceManager;
+    public string channelName = "TestChannel";
+
+    public void Awake()
+    {
+        voiceManager = VivoxVoiceManager.Instance;
+        voiceManager.OnUserLoggedInEvent += OnUserLoggedIn;
+        voiceManager.OnUserLoggedOutEvent += OnUserLoggedOut;
+
+        if (voiceManager.LoginState == LoginState.LoggedIn)
+        {
+            OnUserLoggedIn();
+        }
+    }
+
     public void Start()
     {
+        voiceManager = VivoxVoiceManager.Instance;
         Debug.Log("NetworkStarter.Start()");
     }
 
@@ -16,14 +33,52 @@ public class NetworkStarter : MonoBehaviour
     {
         NetworkManager.Singleton.StartServer();
     }
-    
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        Login();
     }
-    
+
     public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        Login();
+    }
+
+
+    public void Login()
+    {
+        voiceManager.Login(NetworkManager.Singleton.LocalClient.ClientId.ToString());
+    }
+
+    public void JoinChannel()
+    {
+        voiceManager.JoinChannel(channelName, ChannelType.Positional, VivoxVoiceManager.ChatCapability.AudioOnly);
+    }
+
+
+    private void OnUserLoggedIn()
+    {
+        var lobbychannel = voiceManager.ActiveChannels.FirstOrDefault(ac => ac.Channel.Name == channelName);
+        if ((voiceManager && voiceManager.ActiveChannels.Count == 0)
+            || lobbychannel == null)
+        {
+            JoinChannel();
+        }
+        else
+        {
+            if (lobbychannel.AudioState == ConnectionState.Disconnected)
+            {
+                // Ask for hosts since we're already in the channel and part added won't be triggered.
+
+                lobbychannel.BeginSetAudioConnected(true, true, ar => { Debug.Log("Now transmitting into lobby channel"); });
+            }
+        }
+    }
+
+    private void OnUserLoggedOut()
+    {
+        voiceManager.DisconnectAllChannels();
     }
 }
--- Assets/Player/Rotate.cs
+++ Assets/Player/Rotate.cs
@@ -3,12 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
+using VivoxUnity;
 
 public class Rotate : NetworkBehaviour
 {
     public GameObject box;
 
     private Camera cam;
+
+    private VivoxVoiceManager _vivoxVoiceManager;
+
+    private float _nextPosUpdate = 0;
     // Start is called before the first frame update
 
 
@@ -16,6 +21,13 @@ public class Rotate : NetworkBehaviour
 
     // Update is called once per frame
     // send RCP to server to update color and rotation
+
+
+    private void Awake()
+    {
+        if (!isOwner) return;
+        _vivoxVoiceManager = VivoxVoiceManager.Instance;
+    }
 
 
     private void Start()
@@ -35,7 +47,20 @@ public class Rotate : NetworkBehaviour
         box.GetComponent<Renderer>().material.color = Color.Lerp(Color.red, Color.blue, Mathf.PingPong(Time.time, 1));
         Vector3 eulerRotation = new Vector3(cam.transform.eulerAngles.x, cam.transform.eulerAngles.y, cam.transform.eulerAngles.z);
         CmdRotateBoxServerRpc(eulerRotation);
+
+
+        if (!(Time.time > _nextPosUpdate) || _vivoxVoiceManager.LoginState != LoginState.LoggedIn) return;
+        Update3DPosition(gameObject.transform, cam.transform);
+        _nextPosUpdate += 0.3f;
     }
+
+    private void Update3DPosition(Transform listener, Transform speaker)
+    {
+        // _cachedPosition is a custom class with a method that checks the given values against its cached versions. If they differ, it returns true and caches the given ones.
+        _vivoxVoiceManager.TransmittingSession.Set3DPosition(speaker.position, listener.position,
+            listener.forward, listener.up);
+    }
+
 
     [ServerRpc(RequireOwnership = true)]
     void CmdRotateBoxServerRpc(Vector3 rotation)
